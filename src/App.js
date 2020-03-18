0f325b8a5bcf08ae8938894fa2a2674904dba5b8
Daniel <danne_1998@hotmail.se>
2020-03-18 12:26:07 +0100

    app fixed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+
+class App extends Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        return (
+            <h1>hej</h1>
+        );
+    }
+}
+
+export default App;
