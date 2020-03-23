af294a24d9f97d1843970a3ef17b13d875b1bdf0
Daniel <danne_1998@hotmail.se>
2020-03-23 16:53:35 +0100

    added more API calls and commented

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -3,21 +3,82 @@ import * as ApiConfig from "./apiConfig";
 var unirest = require("unirest");
 var req;
 
+/*  This is where API-calls and user info is handled,
+    the API used is "themovieDB" at https://www.themoviedb.org/.
+    User information including username and password is stored in 
+    plaintext txt-files because encrypting is extra work.   */
 class Data {
     constructor(){
     }
     
+    /** * Get a specific movie by movie-ID
+        * @returns { object }  
+    */
     getMovie(id){
-        req = unirest("GET", "https://" + ApiConfig.ENDPOINT + id)
+        req = unirest("GET", "https://" + ApiConfig.ENDPOINT + "movie/" + id)
         return (req
 
             .query({
                 "t": "loadvideo",
                 "q": "60029591",
+                "language":"en-US",
                 "api_key": ApiConfig.API_KEY
             })
         );
-        // MOVIE IMAGE URL = https://image.tmdb.org/t/p/original
+    }
+
+    /** 
+     * Get an array with the top rated movies
+     * @returns { object[] }  
+    */
+    getTopMovies(){
+        req = unirest("GET", "https://" + ApiConfig.ENDPOINT + "movie/" +  "top_rated")
+        return (req
+
+            .query({
+                "t": "loadvideo",
+                "q": "60029591",
+                "language":"en-US",
+                "api_key": ApiConfig.API_KEY
+            })
+        );
+    }
+
+    /** 
+     * Search for movies that are similar to a specific movie
+     * @returns { object[] } 
+    */
+    getSimilarMovies(id){
+        req = unirest("GET", "https://" + ApiConfig.ENDPOINT + "movie/" + id + "/similar")
+        return (req
+
+            .query({
+                "t": "loadvideo",
+                "q": "60029591",
+                "language":"en-US",
+                "api_key": ApiConfig.API_KEY
+            })
+        );
+    }
+
+    /** 
+     * Search for movies based on a query
+     * @returns { object[] } 
+    */
+    searchMovies(query, year, genre){
+        req = unirest("GET", "https://" + ApiConfig.ENDPOINT + "search/movie")
+        return (req
+
+            .query({
+                "t": "loadvideo",
+                "q": "60029591",
+                "language":"en-US",
+                "api_key": ApiConfig.API_KEY,
+                "query": query,
+                "year": year,
+                "with_genres": genre
+            })
+        );
     }
 }
 
