36d21dc467b0c9c244080fbe6d1a4c8202df5770
Daniel <danne_1998@hotmail.se>
2020-03-25 15:02:01 +0100

    Added gucci section and started account infrastructure

---
+++ src/Premium/Premium.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Premium.css";
+
+class Premium extends Component{
+    render(){
+        return(
+        <div className="Premium">
+            <div className="flex_row">
+                <h1 id="premium_text">
+                    GUCCI SECTION UNDER CONSTRUCTION YO
+                </h1>
+            </div>
+        </div>
+        )
+    }
+}
+export default Premium;
