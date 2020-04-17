97fc1321079fa45519b7d9c62abd27446dca06be
Daniel <danne_1998@hotmail.se>
2020-04-17 11:21:55 +0200

    added signup component and moved some css

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link, Redirect } from "react-router-dom";
-import { faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faUser, faLock } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./Login.css";
 
@@ -9,8 +9,7 @@ class Login extends Component {
         super(props);
         this.state = {
             email: "",
-            password: "",
-            signup: this.props.signup
+            password: ""
         }
     }
 
@@ -31,54 +30,12 @@ class Login extends Component {
     }
 
     /** 
-     * signUp
-     * Use this function to set up a new user
-     * @argument { event } e button click
-    */
-    signUp = (e) => {
-        e.preventDefault();
-        this.setupAccount()
-            .then((u) => {
-                console.log(u.user);
-                this.setupDatabase(u.user);
-                this.logIn();
-            })
-            .catch((e) => {
-                console.log(e.message)
-            });
-    }
-
-    /** 
      * loginAccount
      * Use this function to authenticate a user with the firebase-auth-database and log in the user
      * @returns { promise }
     */
     loginAccount = () => {
         return this.props.firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password);
-    }
-
-    /** 
-     * setupAccount
-     * Use this function to create a new user in the firebase-auth-database
-     * @returns { promise }
-    */
-    setupAccount = () => {
-        return this.props.firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password);
-    }
-
-    /** 
-     * Use this function to create the necessary firebase-database references for the current user
-     * @argument { user } user
-    */
-    setupDatabase = (user) => {
-        let databaseRef = this.props.firebase.database().ref('users').child(user.uid);
-        let movieRatingsRef = databaseRef.child('movie-ratings');
-
-        // Dummy values in ratings so it isn't empty
-        movieRatingsRef.child('blacklist').push(0);
-        movieRatingsRef.child('liked-movies').push(0);
-        movieRatingsRef.child('disliked-movies').push(0);
-        movieRatingsRef.child('already-rated').push(0);
     }
 
     /**
@@ -93,16 +50,6 @@ class Login extends Component {
         render(){
             if(this.props.user !== null) return <Redirect to="/swipe"/>;
 
-            switch(this.state.signup){
-                case "0":
-
-                break;
-
-                case "1":
-
-                break;
-            }
-
             return (
                 <div className="Login">
                     <span id="Login_text">
@@ -116,22 +63,21 @@ class Login extends Component {
                     </div>
                     <div className="flex_row">
                         <div className="input_container">
-                            <FontAwesomeIcon icon={faUser} size="2x" className="icon"/>
+                            <FontAwesomeIcon icon={faLock} size="2x" className="icon"/>
                             <input type="password" value={this.state.password} name="password" className="input" placeholder="Password" onChange={this.handleChange}/>
                         </div>
                     </div>
                     <div className="flex_row">
-                        <Link to="/swipe" className="login_Link">
-                            <button className="button"  onClick={this.logIn}>
-                                LOGIN
-                            </button>
-                        </Link>
+                        <button className="auth_button"  onClick={this.logIn}>
+                            LOGIN
+                        </button>
                     </div>
                     <div className="flex_row">
-                        <Link to="/signup" className="login_Link">
-                            <button className="button" onClick={this.signUp}>
-                                SIGN UP
-                            </button>
+                        <p>
+                            Not a member?
+                        </p>
+                        <Link to="/signup">
+                            Sign up now
                         </Link>
                     </div>
                 </div>
---
+++ src/Components/SignUp/SignUp.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { faEnvelope, faUser, faLock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./SignUp.css";
+
+class SignUp extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            confirmPassword: "",
+            username: "",
+            signedUp: 0
+        }
+    }
+
+    /** 
+     * signUp
+     * Use this function to set up a new user
+     * @argument { event } e button click
+    */
+    signUp = (e) => {
+        if(this.state.password !== this.state.confirmPassword || this.state.email === "" || this.state.username === "" || this.state.password === "") console.log("please fill fields");
+        else{
+            e.preventDefault();
+            this.setupAccount()
+                .then((u) => {
+                    console.log(u.user);
+                    this.setupDatabase(u.user);
+                    u.user.updateProfile({displayName: this.state.username});
+                    this.setState({signedUp: 1});
+                })
+                .catch((e) => {
+                    console.log(e.message)
+                });
+        }
+    }
+
+    /** 
+     * setupAccount
+     * Use this function to create a new user in the firebase-auth-database
+     * @returns { promise }
+    */
+    setupAccount = () => {
+        return this.props.firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password);
+    }
+
+    /** 
+     * Use this function to create the necessary firebase-database references for the current user
+     * @argument { user } user
+    */
+    setupDatabase = (user) => {
+        let databaseRef = this.props.firebase.database().ref('users').child(user.uid);
+        let movieRatingsRef = databaseRef.child('movie-ratings');
+
+        // Dummy values in ratings so it isn't empty
+        movieRatingsRef.child('blacklist').push(0);
+        movieRatingsRef.child('liked-movies').push(0);
+        movieRatingsRef.child('disliked-movies').push(0);
+        movieRatingsRef.child('already-rated').push(0);
+    }
+
+    /**
+     * handleChange
+     * Use this function to handle change in an input field
+     * @argument { event } e input field value
+    */
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    render(){
+        if(this.state.signedUp === 1) return <Redirect to="/login"/>;
+        
+        return(
+            <div className="SignUp">
+                <span id="SignUp_text">
+                    SIGN UP
+                </span>
+                <div className="flex_row">
+                    <div className="input_container">
+                        <FontAwesomeIcon icon={faEnvelope} size="2x" className="icon"/>
+                        <input type="email" value={this.state.email} name="email" className="input" placeholder="Email" onChange={this.handleChange}/>
+                    </div>
+                </div>
+                <div className="flex_row">
+                    <div className="input_container">
+                        <FontAwesomeIcon icon={faUser} size="2x" className="icon"/>
+                        <input type="text" value={this.state.username} name="username" className="input" placeholder="Username" onChange={this.handleChange}/>
+                    </div>
+                </div>
+                <div className="flex_row">
+                    <div className="input_container">
+                        <FontAwesomeIcon icon={faLock} size="2x" className="icon"/>
+                        <input type="password" value={this.state.password} name="password" className="input" placeholder="Password" onChange={this.handleChange}/>
+                    </div>
+                </div>
+                <div className="flex_row">
+                    <div className="input_container">
+                        <FontAwesomeIcon icon={faLock} size="2x" className="icon"/>
+                        <input type="password" value={this.state.confirmPassword} name="confirmPassword" className="input" placeholder="Confirm Password" onChange={this.handleChange}/>
+                    </div>
+                </div>
+                <div className="flex_row">
+                    <button className="auth_button" id="SignUp_button" onClick={this.signUp}>
+                        SIGN UP
+                    </button>
+                </div>
+            </div>
+        );
+    }
+    
+}
+export default SignUp;
