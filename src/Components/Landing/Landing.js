0f696c118253ca63913e8763417308291ac41d58
Valle <Valerio.akman@gmail.com>
2020-05-06 15:49:21 +0200

    reconstructed Detailsview and added some application explanation in landing

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -9,7 +9,10 @@ class Landing extends Component{
         return(
             <div className="landingContainer">
                 <p id="landingText">
-                    Welcome to Movie Swiper!
+                    Welcome to Movie Swiper, your movie match maker!
+                </p>
+                <p id="expText">
+                    Vote on movies and recieve movie recommendations based on your votes.<br/>The more you vote, the better the match!
                 </p>
                 <Link to="/login">
                     <button id="landingButton"> Get started </button>
