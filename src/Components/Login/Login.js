91918b66c30b721ae83e92dd9f3d50eb542789a5
Daniel <danne_1998@hotmail.se>
2020-05-07 13:22:10 +0200

    fixed minor routing error

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,7 +6,7 @@ class Login extends Component {
     constructor(props){
         super(props);
         this.state = {
-            user: null
+            user: this.props.data.getUser()
         }
     }
 
